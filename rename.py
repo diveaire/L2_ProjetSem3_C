384cf57eaa8fa4462c8da6fe7462928df1c10d25
diveaire <59625132+diveaire@users.noreply.github.com>
2023-07-02 21:22:38 +0200

    adding menu images

---
+++ rename.py
@@ -0,0 +1,16 @@
+import os
+
+folder = r"rename/"
+# Listing the files of a folder
+world_list = os.listdir(folder)
+
+# rename each file one by one
+for word_folder in word_list:
+	img_list = os.listdir(folder + theme + "/" + word_folder +"/")
+	count = 1
+	for img in img_list:
+		old_name = folder + theme + "/" + word_folder + "/" + img
+		new_name = folder + theme + "/" + word_folder + "/" + str(count) + ".jpg"
+		os.rename(old_name, new_name)
+		count+=1
+print('Rename Done')
